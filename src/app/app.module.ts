618cad255f6751b7cdd029eb71ba713373ff0cbd
Idan <iidan99@hotmail.co.il>
2021-02-01 14:15:28 +0200

    adding the search line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchFieldComponent
   ],
   imports: [
     BrowserModule
